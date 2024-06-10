a6a08da1e7307a9796545316c05fd954e30b944c
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-06-10 16:31:24 +0300

    Add files via upload

--- cezar.py
+++ cezar.py
@@ -1,7 +1,8 @@
-alfavit = {'A':'E', 'B' :'F', 'C' :'G', 'D':'H', 'E':'I','F':'J', 'G':'K',
-           'H':'L', 'I':'M', 'J':'N', 'K':'O', 'L':'P', 'M':'Q', 'N':'R',
-           'O':'S', 'P':'T','Q':'U', 'R':'V', 'S':'W', 'T':'X', 'U':'Y',
-           'V':'Z', 'W':'A', 'X':'B', 'Y':'C', 'Z':'D'}
-
-message = input("Сообщение для шифровки: ").upper()
 # """ Шифр Цезаря """
+alpha = ' abcdefghijklmnopqrstuvwxyz'
+n = 4
+s = input().strip()
+res = ''
+for c in s:
+    res += alpha[(alpha.index(c) + n) % len(alpha)]
+print('Result: "' + res + '"')
